ec5a65c13e8221359e9b712063c5ec3215826831
Drake Rentrop <drakerentrop@gmail.com>
2018-10-16 14:05:38 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,10 +4,11 @@ def hello_t(array)
     yield array[i]
     i +=1
   end
+  array
 end
 
 # call your method here!
-hello_t(["Timm", "Tom", "Jim"]) do |name|
+hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
